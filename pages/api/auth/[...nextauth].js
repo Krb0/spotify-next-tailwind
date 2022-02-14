e44c9be069a88d368bbccc98f89201ae43aa148b
Krb <maurodavidcaceres@gmail.com>
2022-02-14 00:09:43 -0300

    next auth refactoring

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,7 @@
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 
-import { LOGIN_URL } from "../../../lib/spotify";
-import spotifyApi from "../../../lib/spotify";
+import spotifyApi, { LOGIN_URL } from "../../../lib/spotify";
 const refreshAccessToken = async (token) => {
   try {
     spotifyApi.setAccessToken(token.accessToken);
