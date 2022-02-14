1902d5874e772e9e836d5bf7c5dd1dcfe49f2e5c
Krb <maurodavidcaceres@gmail.com>
2022-02-14 00:09:15 -0300

    login page Head added

--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,26 @@
 import React from "react";
-import { getProviders, signIn } from "next-auth/react";
+import { getProviders, signIn, useSession } from "next-auth/react";
+
 import Image from "next/image";
 import SpotifyLogo from "../assets/Spotify-logo.png";
+import Head from "next/head";
 const Login = ({ providers }) => {
+  const { data: session, status } = useSession();
+  console.log(session);
   return (
     <div className="flex flex-col items-center w-screen h-screen justify-center gap-10 bg-[#242424] ">
+      <Head>
+        <title>Spotify | Login </title>
+        <meta name="description" content="A Spotify clone made with NextJS" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <div className="relative w-[200px] h-[120px] sm:w-[400px] sm:h-[240px] ">
-        <Image src={SpotifyLogo} alt="Spotify Logo" layout="fill" />
+        <Image
+          src={SpotifyLogo}
+          alt="Spotify Logo"
+          layout="fill"
+          priority={true}
+        />
       </div>
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
