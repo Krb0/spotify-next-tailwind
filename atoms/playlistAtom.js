9cb68799f49491438825cf181789a1799dc58a9a
Krb <maurodavidcaceres@gmail.com>
2022-02-17 00:33:15 -0300

    Recoil playlist state added

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,8 @@
+import { atom } from "recoil";
+
+export const playlistState = atom({ key: "playlistAtom", default: null });
+
+export const playlistIdState = atom({
+  key: "playlistIdState",
+  default: "0jKVCLiGlVECSJGe1yeQ0q",
+});
