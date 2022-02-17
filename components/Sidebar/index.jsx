9aaca5c3535c05e2d3d12fe7883c11b8fb7dfd51
Krb <maurodavidcaceres@gmail.com>
2022-02-17 00:32:33 -0300

    sidebar minimun width added

--- components/Sidebar/index.jsx
+++ components/Sidebar/index.jsx
@@ -4,7 +4,7 @@ import Playlist from "./Playlist";
 
 const Sidebar = () => {
   return (
-    <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll h-screen scrollbar-hide">
+    <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll h-screen scrollbar-hide space-y-4 min-w-auto sm:min-w-[225px] ">
       <Buttons />
       <Playlist />
     </div>
